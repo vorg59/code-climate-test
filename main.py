459ba99de9e007d832c81f4c25daf92e0e72438e
vorg59 <manas.tanveer6102@gmail.com>
2025-05-07 20:13:33 +0500

    5

--- main.py
+++ main.py
@@ -266,13 +266,3 @@ def menu(death_count):
 
 menu(death_count=0)
 
-def complex_logic():
-    cond_1 = True
-    cond_2 = True
-    cond_3 = True
-    cond_4 = True
-    cond_5 = False
-    cond_6 = False
-    if cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6:
-        return 0
-    return 1 

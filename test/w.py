440d35672e0beb1bc2ed852129fe30d17b18f839
vorg59 <manas.tanveer6102@gmail.com>
2025-05-07 19:48:34 +0500

    2

---
+++ test/w.py
@@ -0,0 +1,10 @@
+def complex_logic():
+    cond_1 = True
+    cond_2 = True
+    cond_3 = True
+    cond_4 = True
+    cond_5 = False
+    cond_6 = False
+    if cond_1 and cond_2 and cond_3 and cond_4 and cond_5 and cond_6:
+        return 0
+    return 1 
